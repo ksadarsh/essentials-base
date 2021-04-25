5d95b385d40434080ab66e266f9b18343e8cd60e
Adarsh Bhat <59079003+ksadarsh@users.noreply.github.com>
2021-04-25 11:04:28 +0530

    Base requirements

---
+++ netinstall-essentials.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt install build-essential intltool pkg-config libalglib-dev libglib2.0-dev libdbus-1-dev libdbus-glib-1-dev libx11-dev libgtk2.0-dev libwnck-dev libgudev-1.0-dev libgtkmm-2.4-dev libssl-dev libnotify-dev gcc make cmake
